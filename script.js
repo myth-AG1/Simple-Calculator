9c56909c85dcc44886cc729bc84f4f0d57b5d555
myth-AG1 <myth.aka.ag1@gmail.com>
2025-09-04 18:16:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('calcForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operator = document.getElementById('operator').value;
+    let result;
+    switch(operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+    }
+    document.getElementById('result').textContent = 'Result: ' + result;
+});
